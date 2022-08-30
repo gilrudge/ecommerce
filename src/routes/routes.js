fce0b733bb140ac4a2ef87e8ccce5cde2b5c6887
Gil Rudge <gilblas07@hotmail.com>
2022-08-29 21:58:37 -0300

    finalização de rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,40 +4,67 @@ const router = express.Router()
 
 const status = 200;
 
-router.get('/produtos/', async (req, res) => { 
-  
-  const productList = await produtos.find({}); 
- res
+
+
+router.get('/produtos/', async (req, res) => {
+
+  const productList = await produtos.find({});
+  res
     .status(status)
     .send(productList);
 });
 
-router.post('/produtos', (req, res) => {
+router.post('/produtos', async (req, res) => {
   const item = req.body
 
   const itemBatata = new produtos({ name: item.name, price: item.price, qtt: item.qtt });
 
-  async function saveItem() {
+  await itemBatata.save();
 
-    await itemBatata.save(); 
-    
-  };
-  saveItem();
-  
   res.send(itemBatata);
 
 });
 
-router.put('/produtos/', (req, res) => {
-  // const id = req.params.id
+router.put('/produtos/:id', async (req, res) => {
+  const id = req.params.id
   const item = req.body;
-  const batata = item.map(item => item);
-  res.send(batata)
+  const teste = await produtos.replaceOne({ _id: id },
+    {
+      name: item.name,
+      price: item.price,
+      qtt: item.qtt
+    });
+
+  res.send({
+    message: "item alterado!"
+  })
+
 
 });
 
-router.delete('/', async (req, res) => {
-  
+// router.patch('/produtos/:id', async (req, res) => {
+
+//   const id = req.params.id;
+//   const item = req.body;
+//   const query = { _id: id };
+
+//   await produtos.findOneAndUpdate(query, { name: item.name })
+
+//   res.send({
+//     message: "item alterado"
+//   })
+
+// });
+
+router.delete('/produtos/:id', async (req, res) => {
+  const id = req.params.id
+
+  await produtos.deleteOne({ _id: id });
+
+  res.send({
+    message: `Item excluído`
+  })
+
 
 });
 
