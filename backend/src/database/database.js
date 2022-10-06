d0aa3ef4ffd0e1d2cfe9ff6ecfffe4d6d92b49da
Gil Rudge <gilblas07@hotmail.com>
2022-10-05 21:57:04 -0300

    troca de past github

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({path:'./.env'})
 // mongoose.connect(`mongodb+srv://teste:1234@testeprodutos.7tqgynq.mongodb.net/?retryWrites=true&w=majority`);
 
+// `mongodb+srv://gil:1234@cluster0.ns5gjl8.mongodb.net/?retryWrites=true&w=majority`
+// mongodb://gil:1234@localhost:27017/mongoGil
+
 async function main() {
-  await mongoose.connect(`mongodb+srv://teste:1234@testeprodutos.7tqgynq.mongodb.net/?retryWrites=true&w=majority`);
+  await mongoose.connect(process.env.MONGO_URL);
 }
 main();
 
